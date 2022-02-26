4a948270a5493c82924feb413e236a2261ddbeb8
Loraine Monteagudo <loraine.monteagudo@gmail.com>
2022-02-26 16:40:15 -0500

    Adding documentation to the  method

--- mqtt_client/messages.py
+++ mqtt_client/messages.py
@@ -1,8 +1,20 @@
 from protos.downlink_pb2 import Downlinks
-from google.protobuf.json_format import MessageToDict
+from google.protobuf.json_format import MessageToJson
 
 def create_downlink_msg(f_port: int, frm_payload: str, priority: str) -> str:
+    '''
+    Helper function to create a downlink message. 
+    Uses protobuf files to represent the structure of the message
+    
+    Example message:
+    {
+        "downlinks": [{
+            "f_port": 15,
+            "frm_payload": "vu8=",
+            "priority": "NORMAL"
+        }]
+    }
+    '''
     downlinks = Downlinks()
-    downlinks.downlink.add(f_port=f_port, frm_payload=frm_payload, priority=priority)
-    d = MessageToDict(downlinks)
-    return str(d)
+    downlinks.downlink.add(f_port=f_port, frm_payload=frm_payload, priority=priority) 
+    return MessageToJson(downlinks)
